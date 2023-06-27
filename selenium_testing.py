7f85a4fb603df8ba28a0bf14aee1592a92f19031
TLJEnthusiast <92526921+TLJEnthusiast@users.noreply.github.com>
2023-06-27 19:16:28 +0200

    Update selenium_testing.py

--- selenium_testing.py
+++ selenium_testing.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
-myoptions = webdriver.ChromeOptions()
-myoptions.binary_location = '/usr/bin/chromium-browser'
-driver = webdriver.Chrome("/home/user2/HK47-virtual-assistant/webdriver/chromedriver_linux64/chromedriver")
+# myoptions = webdriver.ChromeOptions()
+# myoptions.binary_location = '/usr/bin/chromium-browser'
+driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver')
 driver.get("http://www.python.org")
