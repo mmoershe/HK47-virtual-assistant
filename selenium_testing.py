98b7732b8251d2887b30002866c38f916265f0dc
TLJEnthusiast <92526921+TLJEnthusiast@users.noreply.github.com>
2023-06-27 18:29:54 +0200

    ajklsdf

--- selenium_testing.py
+++ selenium_testing.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import os
 
 chrome_options = Options()
 chrome_options.add_argument('--headless')
@@ -7,7 +8,13 @@ chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.binary_location = '/usr/bin/chromium-browser'
 
-driver = webdriver.Chrome('webdriver/chromedriver_linux64/chromedriver', chrome_options=chrome_options)
+# Get the current directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+# Specify the relative path to the Chromedriver
+driver_path = os.path.join(current_dir, 'chromedriver')
+
+
+driver = webdriver.Chrome(driver_path, chrome_options=chrome_options)
 
 driver.get("https://www.selenium.dev/selenium/web/web-form.html")
 
