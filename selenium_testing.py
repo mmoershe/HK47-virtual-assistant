be022977d70f584d05e0f5b65ac28e4be564ea8c
TLJEnthusiast <92526921+TLJEnthusiast@users.noreply.github.com>
2023-06-27 18:13:01 +0200

    Update selenium_testing.py

--- selenium_testing.py
+++ selenium_testing.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(service = Service("webdriver\\chromedriver_linux64\\chromedriver"))
+
 
 driver.get("https://www.selenium.dev/selenium/web/web-form.html")
