2a38786eae35ec6ae6d7b5511fcae9b77e720ba9
TLJEnthusiast <92526921+TLJEnthusiast@users.noreply.github.com>
2023-06-27 18:24:36 +0200

    Update selenium_testing.py

--- selenium_testing.py
+++ selenium_testing.py
@@ -7,7 +7,7 @@ chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.binary_location = '/usr/bin/chromium-browser'
 
-driver = webdriver.Chrome('webdriver/chromedriver_linux64/chromedriver')
+driver = webdriver.Chrome('webdriver/chromedriver_linux64/chromedriver', chrome_options=chrome_options)
 
 driver.get("https://www.selenium.dev/selenium/web/web-form.html")
 
