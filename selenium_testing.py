be33a86fbfe6f3704a69eee65d3286a2031ef874
TLJEnthusiast <92526921+TLJEnthusiast@users.noreply.github.com>
2023-06-27 18:17:04 +0200

    Update selenium_testing.py

--- selenium_testing.py
+++ selenium_testing.py
@@ -1,7 +1,15 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
-driver = webdriver.Chrome(service = Service("webdriver\\chromedriver_linux64\\chromedriver"))
+chrome_options = Options()
+chrome_options.add_argument('--headless')  # Run Chrome in headless mode (without GUI)
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.binary_location = '/usr/bin/chromium-browser'  # Path to Chromium binary
+driver_path = 'webdriver/chromedriver_linux64/chromedriver'  # Path to Chromedriver
 
+driver = webdriver.Chrome(options=chrome_options, executable_path=driver_path)
 
 driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+
+print(driver.title)
