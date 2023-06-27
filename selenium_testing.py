7f7649a1c937d01e2fa350b52d95cfd8f653c38e
TLJEnthusiast <92526921+TLJEnthusiast@users.noreply.github.com>
2023-06-27 18:48:04 +0200

    Update selenium_testing.py

--- selenium_testing.py
+++ selenium_testing.py
@@ -1,21 +1,3 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-import os
 
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
-options.binary_location = '/usr/bin/chromium-browser'
-
-# Get the current directory
-current_dir = os.path.dirname(os.path.abspath(__file__))
-# Specify the relative path to the Chromedriver
-driver_path = os.path.join(current_dir, 'chromedriver')
-
-
-driver = webdriver.Chrome(driver_path, options=options)
-
-driver.get("https://www.selenium.dev/selenium/web/web-form.html")
-
-print(driver.title)
+driver = webdriver.Chrome()
