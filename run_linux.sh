50bb304362d64ba4a9dba9c39f9040e4e7e48d6c
TLJEnthusiast <92526921+TLJEnthusiast@users.noreply.github.com>
2023-11-02 22:31:58 +0100

    added absolute paths

--- run_linux.sh
+++ run_linux.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 
-echo git pull; echo ""
-git pull
+# Define absolute paths
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+VENV_NAME="$SCRIPT_DIR/HK-venv"
 
-VENV_NAME="HK-venv"
+echo "Performing a git pull..."; echo ""
+git -C "$SCRIPT_DIR" pull
 
-echo "Checking if the venv already exists"; echo ""
+echo "Checking if the venv already exists..."; echo ""
 if [ -d "$VENV_NAME" ]; then 
     echo "The venv already exists!"; echo ""
 else    
-    echo "Venv doesnt exist yet. Creating it..."; echo ""
+    echo "Venv doesn't exist yet. Creating it..."; echo ""
     python3 -m venv "$VENV_NAME"
     echo "Venv has been created!"; echo ""
 fi
 
-echo Activating venv; echo ""
+echo "Activating venv..."; echo ""
 source "$VENV_NAME"/bin/activate
 
-echo Installing all the dependencies; echo ""
-pip install -r requirements.txt
+echo "Installing all the dependencies..."; echo ""
+pip install -r "$SCRIPT_DIR/requirements.txt"
 
-echo List of dependencies:; echo ""
+echo "List of dependencies:"; echo ""
 pip freeze
 
 echo ""; echo "Done!"; echo ""
 
 echo "Running the main script now..."; echo ""
-python main.py
+python "$SCRIPT_DIR/main.py"
