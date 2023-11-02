f909d199251ae339b56c42bf2e569d6c7a0ba3e0
TLJEnthusiast <92526921+TLJEnthusiast@users.noreply.github.com>
2023-11-02 21:15:59 +0100

    Update testing.py

--- testing/testing.py
+++ testing/testing.py
@@ -2,10 +2,7 @@ import subprocess
 
 def get_raspberry_pi_temperature():
     result = subprocess.run(['vcgencmd', 'measure_temp'], capture_output=True, text=True)
-    temperature_string = result.stdout.strip()
-    temperature = float(temperature_string.split('=')[1].split("'")[0])
-    return temperature
+    print(result)
+    return result.stdout.strip()
 
-# Example usage
-temperature = get_raspberry_pi_temperature()
-print(f"The temperature of the Raspberry Pi is {temperature} degrees Celsius.")
+print(f"Heat levels: {get_raspberry_pi_temperature()}")
