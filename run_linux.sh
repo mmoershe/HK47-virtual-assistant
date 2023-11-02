2edea6fcc6cbe357c921ad4152133a440f0feb24
TLJEnthusiast <92526921+TLJEnthusiast@users.noreply.github.com>
2023-11-02 19:54:08 +0100

    Update run_linux.sh

--- run_linux.sh
+++ run_linux.sh
@@ -2,24 +2,22 @@
 
 VENV_NAME="HK-venv"
 
-echo "Checking if the venv already exists"
+echo "Checking if the venv already exists"; echo ""
 if [ -d "$VENV_NAME" ]; then 
-    echo "the venv already exists"
+    echo "The venv already exists!"; echo ""
 else    
-    echo "Venv doesnt exist yet. Creating it..."
+    echo "Venv doesnt exist yet. Creating it..."; echo ""
     python3 -m venv "$VENV_NAME"
-    echo "venv has been created!"
+    echo "Venv has been created!"; echo ""
 fi
 
-echo activating venv
+echo Activating venv; echo ""
 source "$VENV_NAME"/bin/activate
-echo activated it 
 
-echo installing all the dependencies s
+echo Installing all the dependencies; echo ""
 pip install -r requirements.txt
 
-echo trying to pip freeze
+echo List of dependencies:; echo ""
 pip freeze
 
-
-echo done
+echo "Done!"
